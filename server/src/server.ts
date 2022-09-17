05d1d876a4fbbff2a1fe9a25953835569941c237
Morgana <morganabagatini@gmail.com>
2022-09-17 14:40:23 -0300

    Creation of projects

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+	return res.send("Olá!")
+});
+
+
+app.listen(3333, () => {
+	console.log("🚀 Aplication running on port 3333");
+});
