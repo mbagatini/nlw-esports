3834416720ca1400a4f38637e2d84734d392cecf
Morgana <morganabagatini@gmail.com>
2022-09-20 14:28:45 -0300

    Craating components for landing page

---
+++ web/src/components/CreateAdBanner.tsx
@@ -0,0 +1,22 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+
+export function CreateAdBanner() {
+	return (
+		<div className="bg-nlw-gradient rounded-lg self-stretch overflow-hidden mt-8 pt-1">
+			<div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+				<div>
+					<strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+					<span className="block text-zinc-300">Publique um anúncio para encontrar novos players!</span>
+				</div>
+
+				<div>
+					<button className="text-white bg-violet-500 hover:bg-violet-600 rounded py-3 px-4 flex items-center gap-3">
+						<MagnifyingGlassPlus size={24} />
+						Publicar anúncio
+					</button>
+				</div>
+			</div>
+
+		</div>
+	)
+}
---
+++ web/src/components/GameBanner.tsx
@@ -0,0 +1,18 @@
+interface GameBannerProps {
+	bannerUrl: string;
+	title: string;
+	adsCount: number;
+}
+
+export function GameBanner(props: GameBannerProps) {
+	return (
+		<a href="" className="relative rounded-lg overflow-hidden">
+			<img src={props.bannerUrl} alt="" />
+
+			<div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute right-0 left-0 bottom-0">
+				<strong className="text-white font-bold block">{props.title}</strong>
+				<span className="text-zinc-300 text-300 block">{props.adsCount} {props.adsCount == 1 ? 'anúncio' : 'anúncios'}</span>
+			</div>
+		</a>
+	)
+}
