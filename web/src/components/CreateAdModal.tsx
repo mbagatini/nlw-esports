22db2793140d684545aa7ecde80336a5e18eade4
Morgana <morganabagatini@gmail.com>
2022-09-22 23:17:59 -0300

    Toggle group: style using pure tailwind

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -85,13 +85,13 @@ export function CreateAdModal({ games }: CreateAdModalProps) {
 							<Label htmlFor="weekDays">Quando costuma jogar?</Label>
 
 							<Toggle.Root type='multiple' onValueChange={setWeekDays} className='grid grid-cols-4 gap-2'>
-								<Toggle.Item value="0" className={`w-8 h-8 rounded ${weekDays.includes('0') ? 'bg-violet-500' : 'bg-zinc-900'}`}>D</Toggle.Item>
-								<Toggle.Item value="1" className={`w-8 h-8 rounded ${weekDays.includes('1') ? 'bg-violet-500' : 'bg-zinc-900'}`}>S</Toggle.Item>
-								<Toggle.Item value="2" className={`w-8 h-8 rounded ${weekDays.includes('2') ? 'bg-violet-500' : 'bg-zinc-900'}`}>T</Toggle.Item>
-								<Toggle.Item value="3" className={`w-8 h-8 rounded ${weekDays.includes('3') ? 'bg-violet-500' : 'bg-zinc-900'}`}>Q</Toggle.Item>
-								<Toggle.Item value="4" className={`w-8 h-8 rounded ${weekDays.includes('4') ? 'bg-violet-500' : 'bg-zinc-900'}`}>Q</Toggle.Item>
-								<Toggle.Item value="5" className={`w-8 h-8 rounded ${weekDays.includes('5') ? 'bg-violet-500' : 'bg-zinc-900'}`}>S</Toggle.Item>
-								<Toggle.Item value="6" className={`w-8 h-8 rounded ${weekDays.includes('6') ? 'bg-violet-500' : 'bg-zinc-900'}`}>S</Toggle.Item>
+									<Toggle.Item value="0" className='w-8 h-8 rounded [&[data-state="on"]]:bg-violet-500 bg-zinc-900'>D</Toggle.Item>
+									<Toggle.Item value="1" className='w-8 h-8 rounded [&[data-state="on"]]:bg-violet-500 bg-zinc-900'>S</Toggle.Item>
+									<Toggle.Item value="2" className='w-8 h-8 rounded [&[data-state="on"]]:bg-violet-500 bg-zinc-900'>T</Toggle.Item>
+									<Toggle.Item value="3" className='w-8 h-8 rounded [&[data-state="on"]]:bg-violet-500 bg-zinc-900'>Q</Toggle.Item>
+									<Toggle.Item value="4" className='w-8 h-8 rounded [&[data-state="on"]]:bg-violet-500 bg-zinc-900'>Q</Toggle.Item>
+									<Toggle.Item value="5" className='w-8 h-8 rounded [&[data-state="on"]]:bg-violet-500 bg-zinc-900'>S</Toggle.Item>
+									<Toggle.Item value="6" className='w-8 h-8 rounded [&[data-state="on"]]:bg-violet-500 bg-zinc-900'>S</Toggle.Item>
 							</Toggle.Root>
 						</div>
 						<div className='flex flex-col gap-2 flex-1'>
