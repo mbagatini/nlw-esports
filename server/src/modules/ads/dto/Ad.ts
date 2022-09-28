bea54992b33de5794709d0e11bbcf746fd55cb38
Morgana <morganabagatini@gmail.com>
2022-09-28 14:19:16 -0300

    Ads - get discord: DTO

--- server/src/modules/ads/dto/Ad.ts
+++ server/src/modules/ads/dto/Ad.ts
@@ -3,3 +3,5 @@ import { Ad as PrismaAd } from "@prisma/client";
 export type Ad = PrismaAd;
 
 export type AdList = Omit<PrismaAd, "hourStart" | "hourEnd"> | { hourStart: string, hourEnd: string };
+
+export type AdDiscord = Pick<PrismaAd, "discord">;
