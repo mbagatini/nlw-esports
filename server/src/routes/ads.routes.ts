40d334cac2a7bd99f9cc2e541e1f0a3af511af51
Morgana <morganabagatini@gmail.com>
2022-09-28 14:21:09 -0300

    Ads - get discord by id: route

--- server/src/routes/ads.routes.ts
+++ server/src/routes/ads.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { listAdsController } from "../modules/ads/useCases/listAds";
+import { getDiscordByAdIdController } from "../modules/ads/useCases/getDiscordByAdId";
 
 export const adsRouter = Router();
 
 adsRouter.get("/", (req, res) => {
 	return listAdsController().handle(req, res);
+});
+
+adsRouter.get("/:id/discord", (req, res) => {
+	return getDiscordByAdIdController().handle(req, res);
 });
